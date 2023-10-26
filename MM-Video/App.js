baa779831d099bd14c9a77830ea53c927f673bb6
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-26 10:38:15 -0300

    no se ve nada

--- MM-Video/App.js
+++ MM-Video/App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Video from './components/VideoPLayer';
+import VideoPLayer from './components/VideoPLayer';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-
-      <StatusBar style="auto" />
-    </View>
+    <div className="App">
+      <VideoPLayer></VideoPLayer>
+      <a>a</a>
+    </div>
   );
 }
 
--- MM-Video/components/VideoPLayer.jsx
+++ MM-Video/components/VideoPLayer.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
-import VideoPlayer from 'react-native-video-controls';
+import React, { useState, useRef } from 'react';
+import ReactPlayer from 'react-player';
 
-const Video = () => {
-return (
-    <VideoPlayer
-    source={{uri: 'https://example.com/video.mp4'}}
-    style={{width: '100%', height: 300}}
-    resizeMode="contain"
-    />
-);
+const VideoPlayer = () => {
+  const [playing, setPlaying] = useState(false);
+  const playerRef = useRef(null);
+
+  const togglePlay = () => {
+    setPlaying(!playing);
+  };
+
+  const stopVideo = () => {
+    if (playerRef.current) {
+      playerRef.current.seekTo(0);
+      setPlaying(false);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Video Player</h1>
+      <ReactPlayer
+        ref={playerRef}
+        url="https://example.com/video.mp4" // Reemplaza con la URL de tu video remoto o ruta local
+        playing={playing}
+        controls={true}
+        width="100%"
+        height="auto"
+      />
+      <div>
+        <button onClick={togglePlay}>{playing ? 'Pause' : 'Play'}</button>
+        <button onClick={stopVideo}>Stop</button>
+      </div>
+    </div>
+  );
 };
 
-export default Video;
+export default VideoPlayer;
