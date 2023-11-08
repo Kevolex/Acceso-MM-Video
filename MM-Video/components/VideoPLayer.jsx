60d5b3ae1a8bea8f8430ae9dafc786b1a5d63d51
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-08 09:22:39 -0300

    pause restart

--- MM-Video/components/VideoPLayer.jsx
+++ MM-Video/components/VideoPLayer.jsx
@@ -1,13 +1,34 @@
 import * as React from 'react';
-import { View, StyleSheet, Button } from 'react-native';
-import { Video, ResizeMode } from 'expo-av';
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { Video } from 'expo-av';
 
 export default function App() {
   const video = React.useRef(null);
-  const [status, setStatus] = React.useState({});
+  const [isPlaying, setIsPlaying] = React.useState(false);
+
+  const handlePlayPause = () => {
+    if (video.current) {
+      if (isPlaying) {
+        video.current.pauseAsync();
+      } else {
+        video.current.playAsync();
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
+
+  const handleRestart = () => {
+    if (video.current) {
+      video.current.setPositionAsync(0); // Reinicia el video al principio
+      video.current.playAsync();
+      setIsPlaying(true);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Video
+        ref={video}
         source={{ uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
         rate={1.0}
         volume={1.0}
@@ -21,8 +42,17 @@ export default function App() {
           if (status.isError) {
             console.error("Error de reproducción de video:", status.error);
           }
+          setIsPlaying(status.isPlaying);
         }}
       />
+      <View style={styles.controls}>
+        <TouchableOpacity style={styles.button} onPress={handlePlayPause}>
+          <Text>{isPlaying ? 'Pause' : 'Play'}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleRestart}>
+          <Text>Restart</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -30,13 +60,22 @@ export default function App() {
 const styles = StyleSheet.create({
   video: {
     width: 700,
-    height: 500
+    height: 500,
   },
-
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  controls: {
+    flexDirection: 'row',
+    marginTop: 20,
+  },
+  button: {
+    marginHorizontal: 10,
+    padding: 10,
+    backgroundColor: '#007BFF',
+    borderRadius: 5,
   },
 });
